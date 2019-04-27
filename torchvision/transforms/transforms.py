d76e13c056eecb197e3fbc580e29573a4e1a9fbe
Sébastien Lange <lange.sebastien@gmail.com>
2019-04-27 14:38:39 +0200

    fix bad reference to self.vertical_flip

--- torchvision/transforms/transforms.py
+++ torchvision/transforms/transforms.py
@@ -813,10 +813,10 @@ class TwentyCrop(object):
         first_ten = F.ten_crop(img, self.size, vertical_flip=False)
         img = F.vflip(img)
         
-        return first_ten + F.ten_crop(img, self.size, self.vertical_flip)
+        return first_ten + F.ten_crop(img, self.size, vertical_flip=False)
 
     def __repr__(self):
-        return self.__class__.__name__ + '(size={0}, vertical_flip={1})'.format(self.size, self.vertical_flip)
+        return self.__class__.__name__ + '(size={0})'.format(self.size)
 
 
 class LinearTransformation(object):
