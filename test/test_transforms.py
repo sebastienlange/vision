a0a1980d34802cb29a01f812349ee288b3812d86
Sébastien Lange <lange.sebastien@gmail.com>
2019-05-06 10:50:35 +0200

    Add test_twenty_crop

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -131,6 +131,38 @@ class Tester(unittest.TestCase):
                 assert len(results) == 10
                 assert expected_output == results
 
+    def test_twenty_crop(self):
+        to_pil_image = transforms.ToPILImage()
+        h = random.randint(5, 25)
+        w = random.randint(5, 25)
+        for single_dim in [True, False]:
+            crop_h = random.randint(1, h)
+            crop_w = random.randint(1, w)
+            if single_dim:
+                crop_h = min(crop_h, crop_w)
+                crop_w = crop_h
+                transform = transforms.TwentyCrop(crop_h)
+                ten_crop = transforms.TenCrop(crop_h,
+                                                vertical_flip=False)
+            else:
+                transform = transforms.TwentyCrop((crop_h, crop_w))
+                ten_crop = transforms.TenCrop((crop_h, crop_w),
+                                                vertical_flip=False)
+
+            img = to_pil_image(torch.FloatTensor(3, h, w).uniform_())
+            results = transform(img)
+            expected_output = ten_crop(img)
+
+            # Checking if TenCrop and TwentyCrop can be printed as string
+            transform.__repr__()
+            ten_crop.__repr__()
+
+            vflipped_img = img.transpose(Image.FLIP_TOP_BOTTOM)
+            expected_output += ten_crop(vflipped_img)
+
+            assert len(results) == 20
+            assert expected_output == results
+            
     def test_randomresized_params(self):
         height = random.randint(24, 32) * 2
         width = random.randint(24, 32) * 2
